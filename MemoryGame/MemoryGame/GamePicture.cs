a4a8bb74e1d54cc3014f4071ab872d43b4cf234a
ConnorBPhillips <cphillips@harding.edu>
2016-12-04 15:46:25 -0600

    added game picture class

---
+++ MemoryGame/MemoryGame/GamePicture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryGame
+{
+    class GamePicture
+    {
+        public GamePicture()
+        {
+            // load up the pictures to a list
+
+        }
+    }
+}
