f8e56e0f45d065f466d8e351a47699a224600b61
ConnorBPhillips <cphillips@harding.edu>
2016-12-04 00:21:12 -0600

    this will help with game board

--- MemoryGame/MemoryGame/GameBoard.cs
+++ MemoryGame/MemoryGame/GameBoard.cs
@@ -6,7 +6,77 @@ using System.Threading.Tasks;
 
 namespace MemoryGame
 {
-    class GameBoard
-    {
+        class GameBoard
+        {
+            private int gridSize;
+            private bool[,] grid;           // Store the on/off state of the grid
+            private Random rand;
+            public const int MaxGridSize = 10;
+            public const int MinGridSize = 3;
+            public int GridSize
+            {
+                get
+                {
+                    return gridSize;
+                }
+                set
+                {
+                    if (value >= MinGridSize && value <= MaxGridSize)
+                    {
+                        gridSize = value;
+                        grid = new bool[gridSize, gridSize];
+                        NewGame();
+                    }
+                }
+            }
+            public GameBoard()
+            {
+                rand = new Random();
+                GridSize = MinGridSize;
+            }
+            public bool GetGridValue(int row, int col)
+            {
+                return grid[row, col];
+            }
+            public void NewGame()
+            {
+                for (int r = 0; r < gridSize; r++)
+                {
+                    for (int c = 0; c < gridSize; c++)
+                    {
+                        // We need to put the logic for setting pictures to grid here
+                        //grid[r, c] = rand.Next(2) == 1;
+                    }
+                }
+            }
+            // Maybe we can use this
+            public void Move(int row, int col)
+            {
+                if (row < 0 || row >= gridSize || col < 0 || col >= gridSize)
+                {
+                    throw new ArgumentException("Row or column is outside the legal range of 0 to " + (gridSize - 1));
+                }
+
+            }
+            // Needs to be changed to fit
+            public bool IsGameOver()
+            {
+                for (int r = 0; r < gridSize; r++)
+                {
+                    for (int c = 0; c < gridSize; c++)
+                    {
+                        if (grid[r, c])
+                        {
+                            return false;
+                        }
+                    }
+                }
+                // All values must be false (off)
+                return true;
+            }
+        }
+
+
     }
+
 }
--- MemoryGame/MemoryGame/GamePage.xaml.cs
+++ MemoryGame/MemoryGame/GamePage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Core;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -13,6 +14,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,7 +28,113 @@ namespace MemoryGame
         public GamePage()
         {
             this.InitializeComponent();
+            CreateGrid();
         }
+        private GameBoard gameBoard = new GameBoard();
+        private SolidColorBrush black = new SolidColorBrush(Windows.UI.Colors.Black);
+        private SolidColorBrush white = new SolidColorBrush(Windows.UI.Colors.White);
+
+        private void boardCanvas_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            int numberOfCells = gameBoard.GridSize;
+            int rectSize = (int)boardCanvas.Width / numberOfCells;
+            //Rectangle rect = sender as Rectangle;
+
+            Point mousePosition = e.GetPosition(boardCanvas);
+            int row = (int)(mousePosition.Y) / rectSize;
+            int col = (int)(mousePosition.X) / rectSize;
+            // var rowCol = (Point)rect.Tag;
+            gameBoard.Move(row, col);
+            // Redraw the board
+            DrawGrid();
+            GameCompleted();
+
+        }
+        private async void GameCompleted()
+        {
+            if (gameBoard.IsGameOver())
+            {
+                MessageDialog msgDialog = new MessageDialog("Congratulations!  You've won!");
+                // Add an OK button
+                msgDialog.Commands.Add(new UICommand("OK"));
+                // Show the message box and wait aynchrously for a button press
+                IUICommand command = await msgDialog.ShowAsync();
+            }
+        }
+        private void CreateGrid()
+        {
+            int numberOfCells = gameBoard.GridSize;
+            // Remove all previously-existing rectangles
+            boardCanvas.Children.Clear();
+
+            //grid = new bool[numberOfCells, numberOfCells];
+            int rectSize = (int)boardCanvas.Width / numberOfCells;
+            //gameBoard.NewGame();
+            // Turn entire grid on and create rectangles to represent it
+            for (int r = 0; r < numberOfCells; r++)
+            {
+                for (int c = 0; c < numberOfCells; c++)
+                {
+                    //grid[r, c] = true;
+
+                    Rectangle rect = new Rectangle();
+
+                    rect.Fill = white;
+                    rect.Width = rectSize + 1;
+                    rect.Height = rect.Width + 1;
+                    rect.Stroke = black;
+
+                    // Store each row and col as a Point
+                    rect.Tag = new Point(r, c);
+                    //rect. += Rect_MouseLeftButtonDown;
+
+                    int x = c * rectSize;
+                    int y = r * rectSize;
+
+                    Canvas.SetTop(rect, y);
+                    Canvas.SetLeft(rect, x);
+
+                    // Add the new rectangle to the canvas' children
+                    boardCanvas.Children.Add(rect);
+                }
+            }
+        }
+        private void DrawGrid()
+        {
+            int index = 0;
+            int numberOfCells = gameBoard.GridSize;
+            // Set colors of each rectangle based on grid values
+            for (int r = 0; r < numberOfCells; r++)
+            {
+                for (int c = 0; c < numberOfCells; c++)
+                {
+                    Rectangle rect = boardCanvas.Children[index] as Rectangle;
+                    index++;
+
+                    if (gameBoard.GetGridValue(r, c))
+                    {
+                        // On
+                        rect.Fill = white;
+                        rect.Stroke = black;
+                    }
+                    else
+                    {
+                        // Off
+                        rect.Fill = black;
+                        rect.Stroke = white;
+                    }
+                }
+            }
+        }
+
+
+
+        private void newGame_Click(object sender, RoutedEventArgs e)
+        {
+            gameBoard.NewGame();
+            DrawGrid();
+        }
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             var navManager = SystemNavigationManager.GetForCurrentView();
